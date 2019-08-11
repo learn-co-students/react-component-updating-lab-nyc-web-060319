d81173746f62f3b7bb70eb854e6609959bc9d449
TimeSmash <TimeSmash@github.com>
2019-08-10 20:51:09 -0400

    Done.

--- src/Controls.js
+++ src/Controls.js
@@ -1,11 +1,17 @@
 import React, { PureComponent } from 'react';
 
+//props
+//  a fxn that updates uI    
+//  gives Controls uI prop    
+// adds new Timer to state.timerIDs */}
+
 class Controls extends PureComponent {
 
   render() {
     return (
       <div>
         <div>
+          {/*                                 run uIS to change App state uI by -1 */}
           <button type="text" onClick={() => this.props.updateIntervalSetting(-1)}>-</button>
           {this.props.updateInterval}
           <button type="text" onClick={() => this.props.updateIntervalSetting(1)}>+</button>
